28cac220cf16fddf2db7ed06dc20faa2f8853377
Abdullah Almarzouq <an36@pdx.edu>
2021-07-10 12:00:07 -0700

    Hello BlueGate Terminal

---
+++ src/js/BLEdevice.js
@@ -0,0 +1,160 @@
+let BLEdevice = new Array();
+let dev_name = new Array();
+let BLEChars = new Array();
+let BLEvals = new Array();
+let conn_devs = 0;
+let selectedDevID;
+let dup_dev = false;
+
+let options = {     //web-bluetooth filters
+    acceptAllDevices: true,
+    optionalServices: ['generic_access']
+}
+
+
+function scanBLE(){
+    if(conn_devs<5){
+        let tempDev;
+        navigator.bluetooth.requestDevice(options)
+        .then(device => { 
+            for(let i=0; i<conn_devs; i++){
+                if(BLEdevice[i].id==device.id){
+                    alert("Device already connected");
+                    dup_dev = true;
+                    return;
+                }
+            }
+
+            tempDev = device;
+            BLEdevice[conn_devs] = device;
+            BLEdevice[conn_devs].addEventListener('gattserverdisconnected', function(){onDisconnect(device.id,0);}); //triggers diconnection event
+            
+            dev_name[conn_devs] = device.name;
+            BLEvals[conn_devs] = "";
+            conn_devs++;
+    
+            return device.gatt.connect();
+        })
+        .then(server => {console.log(server.device); return server.getPrimaryServices();})
+        .then(services => {
+            console.log(services);
+    
+            for(let i=0; i<services.length; i++){
+                return services[i].getCharacteristics();
+            }
+        })
+        .then(characteristics =>{
+            console.log(characteristics);
+            for(let i=0; i<characteristics.length; i++){
+                BLEChars[conn_devs-1] = characteristics[i];
+                return BLEChars[conn_devs-1].startNotifications().then(_=>{
+                    // console.log('> Notifications started');
+                    BLEChars[conn_devs-1].addEventListener('characteristicvaluechanged', BLEread,true);   //triggers BLEread if data received
+                    addTermTab(dev_name[conn_devs-1], BLEdevice[conn_devs-1].id);
+                    // console.log(conn_devs);
+                }).catch(e=>{
+                    if(BLEdevice[conn_devs-1].gatt.connected){
+                        alert("Connection Failure (Char failure): "+e.message);
+                        BLEdevice[conn_devs-1].gatt.disconnect();
+                        // onDisconnect(BLEdevice[conn_devs-1].id,1);
+                    }
+                    console.log("in char catch");
+                })
+            }
+        })
+        .catch(error => {
+            if(dup_dev){
+                dup_dev=false;
+            }
+            else{
+                if(error.message!="User cancelled the requestDevice() chooser."){
+                    if(tempDev){
+                        console.log("in connect catch");
+                        alert("Connection Failure: "+error.message);
+                        if(tempDev.gatt.connected){
+                            tempDev.gatt.disconnect();
+                        }
+                        else{
+                            console.log("calling onDisconnect");
+                            onDisconnect(tempDev.id,1);
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+
+/*Updates the number of connected devices and the list of connected devices when disconnected*/
+function onDisconnect(dev_id,error=0){
+    // console.log("disconnect");
+    if(conn_devs>0){
+        for(let i=0; i<conn_devs; i++){
+            if(BLEdevice[i].id==dev_id){
+                BLEdevice.splice(i,1);
+                BLEChars.splice(i,1);
+                BLEvals.splice(i,1);
+                dev_name.splice(i,1);
+                conn_devs--;
+                
+                if(!error){
+                    alert("Device just got disconnected");
+                }
+                rmTermTab(dev_id);
+            }
+        }
+    }
+}
+
+function disconnectBtn(dev_id){
+    for(let i=0; i<conn_devs; i++){
+        if(BLEdevice[i].id==dev_id){
+            console.log(BLEdevice[i].id);
+            return BLEdevice[i].gatt.disconnect();
+        }
+    }
+}
+
+/* Writes data to connected device.
+ * If a write attempt fails, then writeBLE() will attempt to write
+ * again after 30 milliseconds
+ */
+async function writeBLE(data){
+    // console.log(data);
+    if(conn_devs>0){
+        let encoder = new TextEncoder('utf-8');
+        await BLEChars[conn_devs-1].writeValue(encoder.encode(data))     //writes 'data' to connected device.  if error, then try again after 30ms.
+        .catch((error)=>{
+            // console.log("in writeBLEindex #36: "+error);
+            setTimeout(async function(){await writeBLE(data);},30);
+        });
+    }
+}
+
+/* This function gets triggered after cBLE notification.
+ * This function reads the value sent by connected device
+ * and parses the value for Volume Level, number of taps,
+ * scanned MAC addresses, and for x,y,z acceleration values.
+ */
+async function BLEread(event){
+    var i,j;
+    if(conn_devs>0){
+        let this_dev_id = event.currentTarget.service.device.id;
+        let this_dev_index;
+        for(let i=0; i<conn_devs; i++){
+            if(BLEdevice[i].id==this_dev_id){
+                this_dev_index = i;
+            }
+        }
+
+        BLEvals[this_dev_index] = "";
+
+        for(j=0; j<BLEChars[this_dev_index].value.byteLength; j++){    //read bytes from received value
+                BLEvals[this_dev_index] += String.fromCharCode(BLEChars[this_dev_index].value.getUint8(j)); //convert value to string
+        }
+
+        console.log(BLEvals[this_dev_index]);
+        terminalLog(0, "terminal1", new Date().toLocaleTimeString(), dev_name[this_dev_index], BLEvals[this_dev_index])
+        // console.log(event.currentTarget.service.device.id);
+    }
+}
---
+++ src/js/UIcontrol.js
@@ -0,0 +1,106 @@
+
+
+
+let connbtn = document.getElementById("connbtn");
+connbtn.onclick = scanBLE;
+
+let discobtn = document.getElementById("discobtn");
+// discobtn.onclick = function(){onDisconnect(selectedDevID)};
+discobtn.onclick = function(){disconnectBtn(selectedDevID)};
+
+let ServsInput = document.getElementById("ServsInput");
+ServsInput.oninput = rmOptionalServs;
+
+let optionalServs = document.getElementById("optionalServs");
+
+let sendbtn = document.getElementById("sendbtn");
+sendbtn.onclick = sendBLE;
+
+let sendtext = document.getElementById("sendtext");
+sendtext.addEventListener("keyup",function(e){
+    if(e.key === 'Enter'){
+        sendBLE();
+    }
+});
+
+let terminalTabs = document.getElementById("tab");
+
+let addRmServsbtn = document.getElementById("addRmServsbtn");
+addRmServsbtn.onclick = addOptionalServs;
+
+
+function sendBLE(){
+    if(sendtext.value.length>0){
+        writeBLE(sendtext.value);
+        terminalLog(1, "terminal1", new Date().toLocaleTimeString(), "All", sendtext.value);
+        sendtext.value = "";
+    }
+}
+
+function addTermTab(device_name, device_id){
+    terminalTabs.innerHTML+="<button id=\""+device_id+"\">"+device_name+"</button>";
+    document.getElementById(device_id).onclick = function(){
+        selectedDevID=device_id;
+    }
+}
+
+function rmTermTab(device_id){
+    let dev_tab = document.getElementById(device_id)
+    if(dev_tab){
+        dev_tab.remove();
+    }
+}
+
+function addOptionalServs(){
+    if(ServsInput.value.length>0){
+        let temp = ServsInput.value;
+        console.log(temp);
+
+        if(addRmServsbtn.innerHTML=="Add"){
+            if(ServsInput.value.startsWith('0x')){
+                temp = parseInt(ServsInput.value,16);
+            }
+            
+            if(!(options.optionalServices.includes(temp))){
+                options.optionalServices.push(temp);
+                optionalServs.innerHTML+="<option id=\""+ServsInput.value+"\" value=\""+ServsInput.value+"\">";
+            }
+        }
+        else{
+            options.optionalServices.pop(temp);
+            document.getElementById(temp).remove();
+            addRmServsbtn.innerHTML="Add";
+        }
+    }
+
+    ServsInput.value = "";
+    console.log(options.optionalServices);
+}
+
+function rmOptionalServs(){
+    if(ServsInput.value.length>0){
+        let temp = ServsInput.value;
+        if(ServsInput.value.startsWith('0x')){
+            temp = parseInt(ServsInput.value,16);
+        }
+        
+        if(options.optionalServices.includes(temp)){
+            addRmServsbtn.innerHTML="Remove";
+        }
+        else{
+            addRmServsbtn.innerHTML="Add";
+        }
+    }
+}
+
+function terminalLog(sending=0,term_id, curr_time, curr_dev_name,val){
+    let curr_teminal = document.getElementById(term_id);
+
+    if(!sending){
+        curr_teminal.innerHTML+="["+curr_time+"] "+curr_dev_name+": "+val+"&#10;";
+    }
+    else{
+        curr_teminal.innerHTML+="["+curr_time+"] sent to "+curr_dev_name+": "+val+"&#10;";
+    }
+    curr_teminal.scrollTop = curr_teminal.scrollHeight;
+}
