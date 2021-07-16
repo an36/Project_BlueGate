4641d903ce42d693316d657262d56bb690283f35
Abdullah Almarzouq <an36@pdx.edu>
2021-07-16 13:48:31 -0700

    renamed asset

--- src/js/UIcontrol.js
+++ src/js/UIcontrol.js
@@ -105,7 +105,7 @@ function updateDevList(add=0,BLEdev, BLEchar){
 
         let tempDevIndex = dev_name.indexOf(BLEdev.name);
     
-        devlistInner.innerHTML += "<a class=\""+BLEdev.id+"\"><b>"+BLEdev.name+"<img onclick=\"disconnectBtn(this);\" name=\""+BLEdev.id+"\" src=\"/src/assets/Red-incorrect-icon-button-on-transparent-background-PNG-1.png\" width=\"17\" height=\"17\"></b>Properties: "+curr_properties+"<br><br><br><br></a>";
+        devlistInner.innerHTML += "<a class=\""+BLEdev.id+"\"><b>"+BLEdev.name+"<img onclick=\"disconnectBtn(this);\" name=\""+BLEdev.id+"\" src=\"/src/assets/discon.PNG\" width=\"17\" height=\"17\"></b>Properties: "+curr_properties+"<br><br><br><br></a>";
         document.getElementsByClassName(BLEdev.id)[0].getElementsByTagName("b")[0].style.color="var(--terminal"+tempDevIndex+"Clr)";
     }
     else{
@@ -254,7 +254,7 @@ document.body.onclick = function(e){
             TermSettingsElement = true;
         }
     }
-    
+
     if(e.target.id == "TerminalSettingsbtn" || e.target.id =="TerminalSettings" || TermSettingsElement){
         return;
     }
